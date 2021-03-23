40fe7fd519827e623bc4bed27361c517dabf9cb8
rui-watanabe <wataru610806@gmail.com>
2021-03-23 22:03:56 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    '@typescript-eslint/no-unused-vars': 'error',
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    'import/extensions': [
+      'error',
+      { extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'] },
+    ],
+    'react/prop-types': 'off',
+    'spaced-comment': ['error', 'always', { markers: ['/ <reference'] }],
+    'prettier/prettier': 'error',
+    'no-console': 'off',
+    'react/no-array-index-key': 'off',
+    'no-param-reassign': [
+      'error',
+      { props: true, ignorePropertyModificationsForRegex: ['state'] },
+    ],
+  },
+};
